03d7fbab023f83d66103d9a75d3ba17c9345fd82
Antonio Mourão <antonio.smsobrinho@gmail.com>
2023-11-04 18:33:54 -0300

    Wire delete product into smoketest.sh

--- test/nex-smoketest.sh
+++ test/nex-smoketest.sh
@@ -52,3 +52,7 @@ ID=$(echo ${ORDER_ID} | jq '.id')
 # Test: Get Order back
 echo "=== Getting Order ==="
 curl -s "${STD_APP_URL}/orders/${ID}" | jq .
+
+#Test: Delete Product
+echo "=== Deleting Product ==="
+curl -X DELETE "${STD_APP_URL}/products/the_odyssey"
